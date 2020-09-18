7fe6ff58f95085e72a3c76b0672dc1a10971a847
tuxedlinux <60256394+tuxedlinux@users.noreply.github.com>
2020-09-18 19:06:59 +0530

    Added features

--- bot.py
+++ bot.py
@@ -15,6 +15,18 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send(f':ping_pong: | **Pong!** Client Side Ping Latency took {round(client.latency * 1000)}ms!')
+    
+@client.command()
+async def code(ctx):
+    await ctx.send(f'Xenial\'s source code is avalaible at GitHub: https://github.com/tuxedlinux/Xenial/')
+
+@client.command()
+async def invite(ctx):
+    await ctx.send(f'If you want to invite Xenial to your server, use this link: https://discord.com/api/oauth2/authorize?client_id=756471287442047056&permissions=8&scope=bot')
+    
+@client.command()
+async def about(ctx):
+    await ctx.send(f'Xenial is a Discord bot by tuxed#1064, written in Python3. Licensed under MIT,  meaning that it\'s open-source and you are free to distribute your own modifications of the bot.')
 
 @client.command()
 async def unload(ctx, extension):
