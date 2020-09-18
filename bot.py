d8dd1d5fc727f2a05cc3e85d3a171386c87bc21a
tuxedlinux <60256394+tuxedlinux@users.noreply.github.com>
2020-09-18 17:46:48 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,17 +13,31 @@ vowels = ["a","e","i","o","u","y"]
 consonants = ["b","c","d","f","g","h","j","k","l","m", \
                  "n","p","q","r","s","t","v","w","x","z"]
 
-client = commands.Bot(command_prefix = '>>')
+client = commands.Bot(command_prefix = '>!')
 
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online,
-    activity=discord.Game(f"Type >>help to get the list of commands"))
+    activity=discord.Game(f"Type >!help to get the list of commands"))
     print('Xenial Bot is online!')
     
 @client.command()
+async def hello(ctx):
+    await ctx.send(random.choice(greetings))
+    
+@client.command()
+async def rubbish(ctx):
+    sentence = ""
+    for i in range(random.randrange(3,7)):
+        word = str()
+        for j in range(random.randrange(1,5)):
+            word = word + random.choice(consonants) + random.choice(vowels)
+        sentence = sentence + word + " "
+    await ctx.send(sentence.capitalize().rstrip() + random.choice(["!","?","."]))
+    
+@client.command()
 async def ping(ctx):
-    await ctx.send(f':ping_pong: | Pong! Client Side Ping Latency took {round(client.latency * 1000)}ms!')
+    await ctx.send(f':ping_pong: | **Pong!** Client Side Ping Latency took {round(client.latency * 1000)}ms!')
 
 @client.command()
 async def unload(ctx, extension):
