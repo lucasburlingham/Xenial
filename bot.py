7723f79b00d2ed57e216b8965b3282c433201078
tuxedlinux <60256394+tuxedlinux@users.noreply.github.com>
2020-09-19 17:02:49 +0530

    added so many feautres !

--- bot.py
+++ bot.py
@@ -6,16 +6,45 @@ from discord.ext.commands import has_permissions
 
 client = commands.Bot(command_prefix = 'a!')
 
+hello = ["Hello!", "Hallo!", "Bonjour!", "Ciao!", "¡Hola!",
+             "Hej!", "Ahoj!", "Aloha!", "Привет!", "Χαίρετε!",
+             "你好!", "こんにちは!", "여보세요!", "!مرحبا", "हैलो!"]
+
+predict = ["
+
+vowels = ["a","e","i","o","u","y"]
+
+consonants = ["b","c","d","f","g","h","j","k","l","m", \
+                 "n","p","q","r","s","t","v","w","x","z"]
+
 @client.event
 async def on_ready():
     await client.change_presence(status=discord.Status.online,
-    activity=discord.Game(f"Type a!help to get the list of commands | Playing on {len(client.guilds)} guilds"))
+    activity=discord.Game(f"Type \'a!help\' to get the list of commands | Playing on {len(client.guilds)} guilds"))
     print('Xenial Bot is online!')
+           
+@client.command()
+async def rubbish(ctx):
+    sentence = ""
+    for i in range(random.randrange(3,7)):
+        word = str()
+        for j in range(random.randrange(1,5)):
+            word = word + random.choice(consonants) + random.choice(vowels)
+        sentence = sentence + word + " "
+    await ctx.send(sentence.capitalize().rstrip() + random.choice(["!","?","."]))
     
 @client.command()
 async def ping(ctx):
     await ctx.send(f':ping_pong: | **Pong!** Client Side Ping Latency took {round(client.latency * 1000)}ms!')
-    
+
+@client.command()
+async def hello(ctx):
+    await ctx.send(random.choice(hello))
+               
+@client.command()
+async def predict(ctx):
+    await ctx.send(random.choice(predict))
+              
 @client.command()
 async def code(ctx):
     await ctx.send(f'Xenial\'s source code is avalaible at GitHub: https://github.com/tuxedlinux/Xenial/')
