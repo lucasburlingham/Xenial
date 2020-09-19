e0c6ecf393819634bfe1c1f54ca588c42dfaff32
tuxedlinux <60256394+tuxedlinux@users.noreply.github.com>
2020-09-19 15:18:00 +0530

    Updated

--- cogs/errorcog.py
+++ cogs/errorcog.py
@@ -15,14 +15,14 @@ class Error(commands.Cog):
     async def on_command_error(self,ctx: commands.Context,error):
 
         if isinstance(error, commands.CommandNotFound):
-            embed = discord.Embed(description=":x: │ The command you typed does not exist...... maybe run the command **>!help**, which will give you a bunch of commands.",colour=discord.Colour.purple())
+            embed = discord.Embed(description=":x: │ The command you typed does not exist. Run the command **a!help**, which will give you a list of commands.",colour=discord.Colour.purple())
             return await ctx.send(embed=embed)
         if isinstance(error, commands.MissingPermissions):
             embed = discord.Embed(description=f":x: │ Sorry, {ctx.message.author.mention}, but you do not have the required permissions.",colour=discord.Colour.purple())
             return await ctx.send(embed=embed)
         if not (ctx.command.name, type(error)) in ignore_command_errors:
             if isinstance(error, commands.MissingRequiredArgument):
-                embed = discord.Embed(description=f":x: │ What you typed was not complete. Maybe add some arguments?",colour=discord.Colour.purple())
+                embed = discord.Embed(description=f":x: │ What you typed was not complete. Add some arguments?",colour=discord.Colour.purple())
                 return await ctx.send(embed=embed)
         if isinstance(error,commands.CommandOnCooldown):
             await ctx.send("Please **wait** before using this command!")
